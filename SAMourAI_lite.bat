f9430ab5a8b99cd6c536d3d9a31e29950294154d
cgalinier <co.galinier@hotmail.fr>
2025-10-20 14:13:10 +0200

    Add files via upload

--- SAMourAI_lite.bat
+++ SAMourAI_lite.bat
@@ -1,15 +1,8 @@
 @echo off
-REM -------------------
-REM SAMourAI Launcher -
-REM -------------------
-
-REM --- Set UTF-8 code page for special characters ---
 chcp 65001 >nul
-
-REM --- Set text color to green ---
 color 03
-
 cls
+
 echo.
 echo ╔──────────────────────╗
 echo │╔═╗╔═╗╔╦╗┌─┐┬ ┬┬─┐╔═╗╦│
@@ -19,53 +12,28 @@ echo ╚──────────────────────╝
 echo      /Lite version/
 echo ▬▬[════════════════════ﺤ 
 echo.
+echo [Initializing system...]
 echo.
-echo                           [Initializing system...]
-echo.
-timeout /t 1 /nobreak >nul
 
-REM --- Detect the directory of the script ---
 SET "PROJECT_PATH=%~dp0"
 
-REM --- Automatically detect the virtual environment ---
 SET "VENV_PATH=%PROJECT_PATH%samourai_env\Scripts\activate.bat"
 
-REM --- Check if the virtual environment exists ---
 IF NOT EXIST "%VENV_PATH%" (
     echo [ERROR] Virtual environment not found: "%VENV_PATH%"
-    echo          Please create the samourai_env environment before running this script.
-    echo.
     pause
     exit /b
 )
 
-echo [OK] Virtual environment detected
-REM --- Activate the virtual environment ---
 call "%VENV_PATH%"
 
-REM --- Path to the image_dir folder ---
 SET "IMAGE_DIR=%PROJECT_PATH%image_dir"
+if not exist "%IMAGE_DIR%" mkdir "%IMAGE_DIR%"
 
-REM --- Model name ---
-SET "MODEL_NAME=sam2.1-hiera-tiny"
+echo [Launching SAMourAI Interface...]
 
-REM --- Create the image_dir folder if it does not exist ---
-if not exist "%IMAGE_DIR%" (
-    echo [OK] Creating image_dir folder
-    mkdir "%IMAGE_DIR%"
-) else (
-    echo [OK] image_dir folder ready
-)
-
-echo [OK] Model: %MODEL_NAME%
-echo.
-echo                           [Launching SAMourAI Interface...]
-echo.
-
-REM --- Launch the GUI interface ---
-python "%PROJECT_PATH%gui_cpu.py" "%IMAGE_DIR%" %MODEL_NAME%
+python "%PROJECT_PATH%launchers\launcher_cpu.py"
 
 echo.
-echo                           [Session ended]
-echo.
+echo [Session ended]
 pause
