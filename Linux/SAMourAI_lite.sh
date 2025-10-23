492ed8c99468698509cc31726e997037ef43b1c7
cgalinier <co.galinier@hotmail.fr>
2025-10-23 09:35:02 +0200

    Add files via upload

--- Linux/SAMourAI_lite.sh
+++ Linux/SAMourAI_lite.sh
@@ -6,13 +6,13 @@ echo "│╔═╗╔═╗╔╦╗┌─┐┬ ┬┬─┐╔═╗╦│"
 echo "│╚═╗╠═╣║║║│ ││ │├┬┘╠═╣║│"
 echo "│╚═╝╩ ╩╩ ╩└─┘└─┘┴└─╩ ╩╩│"
 echo "╚──────────────────────╝"
-echo "     /Lite version/"
+echo "     /lite version/"
 echo "▬▬[════════════════════- "
 echo
 echo "[Initializing system...]"
 echo
 
-PROJECT_PATH="$(cd "$(dirname "$0")/.." && pwd)"
+PROJECT_PATH="$(cd "$(dirname "$0")" && pwd)"
 VENV_PATH="$PROJECT_PATH/samourai_env/bin/activate"
 
 if [ ! -f "$VENV_PATH" ]; then
@@ -27,7 +27,7 @@ source "$VENV_PATH"
 IMAGE_DIR="$PROJECT_PATH/image_dir"
 mkdir -p "$IMAGE_DIR"
 
-echo "[Launching SAMourAI Lite Interface...]"
+echo "[Launching SAMourAI CPU Interface...]"
 python "$PROJECT_PATH/launchers/launcher_cpu.py"
 
 echo
